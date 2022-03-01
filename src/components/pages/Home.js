1df9aec2d95dc8e40ffcdf5c27771dee055687a7
Fouad <r0726732@student.thomasmore.be>
2022-03-01 22:03:10 +0100

    firebase install en services

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,15 @@
-import geneve from '../../Images/HomePage/geneve.jpg'
-export function Home(){
-    return(
-        <div className={"row"}>
+export function Home() {
+    return (<div className={"row"}>
             <div className={"bs-tooltip-bottom"}>
                 <h3>Welcome to Autoshop</h3>
                 <p>Order your car</p>
             </div>
-            <img src={geneve} alt="auto Terre" height="888" width="1853"/>
+            <img
+                src={"https://firebasestorage.googleapis.com/v0/b/autoshop-b6af9.appspot.com/o/HomePage%2Fgeneve.jpg?alt=media&token=3270e718-0947-437e-b8fb-ef755b5e89f2"}
+                alt="auto Terre" height="888" width="1853"/>
         </div>
 
     )
 }
+
 export default Home;
